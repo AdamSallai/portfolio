c2c369da2a0be5c9fa70cb2ecdc860decf5b9c07
Adam Sallai <yadam1998@gmail.com>
2020-12-04 21:25:01 +0100

    add links to navbar that redirect to the given routes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
 
   return (
     <div>
-      Navbar
+      <Link to="/">About</Link>
+      <Link to="/skillset">Skillset</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contact</Link>
     </div>
   );
 };
