111a60f5cf6a6660aa22f0f92f06a592ba87af1c
Adam Sallai <yadam1998@gmail.com>
2020-12-04 21:19:52 +0100

    add react router package and define the routes of the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import About from './components/about/About';
-import Contact from './components/contect/Contact';
-import Navbar from './components/navbar/Navbar';
-import Projects from './components/project/Projects';
-import Skillset from './components/skillset/Skillset';
-import "./style/App.scss"
+import React from "react";
+import About from "./components/about/About";
+import Contact from "./components/contect/Contact";
+import Navbar from "./components/navbar/Navbar";
+import Projects from "./components/project/Projects";
+import Skillset from "./components/skillset/Skillset";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./style/App.scss";
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <About />
-      <Skillset />
-      <Projects />
-      <Contact />
+      <Router>
+        <Navbar />
+        <Route path="/" exact component={About} />
+        <Route path="/skillset" exact component={Skillset} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/contact" exact component={Contact} />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
