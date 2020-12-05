4765cca897d8626d3ac2bf281710c0c337f07415
Adam Sallai <yadam1998@gmail.com>
2020-12-05 22:35:20 +0100

    add project boxes with the data to /projects route

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -3,7 +3,18 @@ import { ProjectBlueprint } from "./Data";
 
 const Project: React.FunctionComponent<{data: ProjectBlueprint}> = ({data}) => {
   return (
-    <div>
+    <div className="project-card">
+      <img className="project-image" src={data.pictureURL} alt="" />
+      <div className="project-textbox">
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+        <div>
+          { data.github.map(link => <a href={link} />) }
+        </div>
+        <div>
+          { data.technologies.map(technology => <p>{technology}, </p>) }
+        </div>
+      </div>
     </div>
   )
 }
