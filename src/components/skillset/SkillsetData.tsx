98c251175e752948a6cac69c810df7bf75b3f429
Adam Sallai <yadam1998@gmail.com>
2020-12-07 21:59:48 +0100

    add basic data for skillset page

---
+++ src/components/skillset/SkillsetData.tsx
@@ -0,0 +1,28 @@
+interface skillsetBlueprint {
+  image: string,
+  title: string,
+  description: Array<string>
+}
+
+export const skillsetData: Array<skillsetBlueprint> = [
+  {
+    image: "",
+    title: "JavaScript",
+    description: ["ES6", "TypeScript", "DOM manipulation", "fetch", "callbacks"]
+  },
+  {
+    image: "",
+    title: "React",
+    description: ["Hooks", "useContext", "Redux", "styled components"]
+  },
+  {
+    image: "",
+    title: "Java",
+    description: ["OOP", "Spring boot", "Hibernate", "Spring Security"]
+  },
+  {
+    image: "",
+    title: "Python",
+    description: ["Python", "Flask and Jinja basics"]
+  }
+]
