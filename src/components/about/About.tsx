e1779d2a042ca0c60dec2c1f218212806c6aa504
Adam Sallai <yadam1998@gmail.com>
2020-12-10 19:50:07 +0100

    add line breaks and better font-sizes for better responsiveness

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div className="about">
       <h1>About me</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse voluptas accusamus recusandae voluptatibus quo vero non, aspernatur doloremque quam sunt cupiditate. Alias nam fugiat dolor a pariatur repudiandae aliquid? Ipsum!</p>
+      <p>
+        The first time I learned about programming was in highschool, and I
+        realized soon that it is what I want to do in my future. Currently I am
+        a student at Codecool, learning full-stack development, and searching
+        for my future job.
+        <br />
+        <br />
+        In this one year at Codecool I got familiar with several programming
+        languages and frameworks while making projects with different teams or
+        by myself. I learned a lot about both frontend and backend. Currently I
+        tend to enjoy working more on frontend projects using JavaScript with
+        React, and I would like to get familiar with more frontend technologies
+        (like Vue.js, TypeScript, etc) in the future.
+        <br />
+        <br />
+        Competencies: JavaScript, React.js, CSS, Java, Spring boot, Python,
+        Flask, PostgreSQL, Git
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
